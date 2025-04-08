a7cb0e89eb795f2e982c574449e9bf507b5333d7
Stainamou <margussten@gmail.com>
2025-04-08 09:38:46 +0300

    exercise 2

--- src/components/MediaRow.jsx
+++ src/components/MediaRow.jsx
@@ -1,7 +1,12 @@
 import PropTypes from 'prop-types';
 
 const MediaRow = (props) => {
-  const {item} = props;
+  const {item, setSelectedItem} = props;
+
+  const handleClick = () => {
+    setSelectedItem(item);
+  };
+
   return (
     <tr>
       <td>
@@ -12,22 +17,16 @@ const MediaRow = (props) => {
       <td>{new Date(item.created_at).toLocaleString('fi-FI')}</td>
       <td>{item.filesize}</td>
       <td>{item.media_type}</td>
+      <td>
+        <button onClick={handleClick}>View</button>
+      </td>
     </tr>
   );
 };
 
 MediaRow.propTypes = {
-  item: PropTypes.shape({
-    media_id: PropTypes.number.isRequired,
-    user_id: PropTypes.number.isRequired,
-    filename: PropTypes.string.isRequired,
-    thumbnail: PropTypes.string.isRequired,
-    filesize: PropTypes.number.isRequired,
-    media_type: PropTypes.string.isRequired,
-    title: PropTypes.string.isRequired,
-    description: PropTypes.string,
-    created_at: PropTypes.string.isRequired,
-  }).isRequired,
+  item: PropTypes.object.isRequired,
+  setSelectedItem: PropTypes.func.isRequired,
 };
 
 export default MediaRow;
